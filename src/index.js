c8f8a4c8dbe020f0680ff448451dad92ee4ed509
Liger <jtadub@gmail.com>
2020-08-11 19:46:20 +1200

    Add image carousel

--- src/index.js
+++ src/index.js
@@ -15,3 +15,52 @@ const toggleMenu = function () {
 }
 
 siteNavButton.addEventListener('click', toggleMenu);
+
+const images = document.querySelectorAll('.slide-image');
+
+let counter = 1;
+
+const slideLeft = function (imageList) {
+  if (counter === imageList.length - 1) {
+    counter = 1;
+  }else{
+    counter += 1;
+  }
+
+  imageList.forEach((image) => {
+    image.style = `transform: translateX(${counter * 960 * -1}px)`
+  })
+}
+
+const slideRight = function (imageList) {
+  console.log(counter)
+  if (counter === 1) {
+    counter = imageList.length - 1;
+  }else {
+    counter -= 1;
+  }
+
+  imageList.forEach((image) => {
+    image.style = `transform: translateX(${counter * 960 * -1}px)`
+  })
+}
+
+const leftArrowButton = document.querySelector('#image-carousel-left');
+leftArrowButton.addEventListener('click', () => {slideLeft(images)});
+
+const rightArrowButton = document.querySelector('#image-carousel-right');
+rightArrowButton.addEventListener('click', () => {slideRight(images)});
+
+leftArrowButton.addEventListener('mousedown', () => {
+  leftArrowButton.classList.add('clicked');
+  setTimeout(() => {
+    leftArrowButton.classList.remove('clicked')
+  }, 150)
+});
+
+rightArrowButton.addEventListener('mousedown', () => {
+  rightArrowButton.classList.add('clicked');
+  setTimeout(() => {
+    rightArrowButton.classList.remove('clicked')
+  }, 150)
+})
